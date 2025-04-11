a56f900afecf7e5d59d49531b05824d93726ecb6
GieQx <99009247+GieQx@users.noreply.github.com>
2025-04-11 14:55:56 +0800

    Update Header.tsx

--- src/components/homepage/Header.tsx
+++ src/components/homepage/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
       <div className="container mx-auto px-4 flex items-center justify-between">
         <Link to="/" className="flex items-center">
           <h1 className="text-2xl font-bold text-gray-900 dark:text-white md:text-3xl">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Philippine_Statistics_Authority.svg/1059px-Philippine_Statistics_Authority.svg.png" alt="Logo" className="h-8 w-auto mr-2" />PSA Events
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Philippine_Statistics_Authority.svg/1059px-Philippine_Statistics_Authority.svg.png" alt="Logo" className="h-8 w-auto mr-2" />
           </h1>
         </Link>
         <div className="flex items-center gap-4">
