f9431a8ebc29b989c5fb7236a9a3704e2b0918ec
GieQx <99009247+GieQx@users.noreply.github.com>
2025-04-11 14:58:57 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 
 import { useEffect, useState } from "react";
-// import { ParticleBackground } from "@/components/ParticleBackground";
+import { ParticleBackground } from "@/components/ParticleBackground";
 import { HeroSection } from "@/components/homepage/HeroSection";
 import { EventsSection } from "@/components/homepage/EventsSection";
 import { AboutSection } from "@/components/homepage/AboutSection";
@@ -43,11 +43,11 @@ const IndexPage = () => {
 
   return (
     <div className="relative min-h-screen overflow-x-hidden">
-     <--! <ParticleBackground 
+     <ParticleBackground 
         color="#3b82f6" 
         particleCount={100}
         className="absolute inset-0 -z-10 opacity-70" 
-      /> -->
+      />
       
       <Header />
 
